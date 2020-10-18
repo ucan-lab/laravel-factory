c30a6a69997fa0726e86688ecbd73a571a3950de
ucan-lab <35098175+ucan-lab@users.noreply.github.com>
2020-10-18 22:46:27 +0900

    wip

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -24,10 +24,49 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name,
+            'account_status' => 'active',
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
+    }
+
+    /**
+     * @return Factory
+     */
+    public function preRegistered(): Factory
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'account_status' => 'pre_registered',
+                'email_verified_at' => null,
+            ];
+        });
+    }
+
+    /**
+     * @return Factory
+     */
+    public function active(): Factory
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'account_status' => 'active',
+                'email_verified_at' => now(),
+            ];
+        });
+    }
+
+    /**
+     * @return Factory
+     */
+    public function suspended(): Factory
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'account_status' => 'suspended',
+            ];
+        });
     }
 }
